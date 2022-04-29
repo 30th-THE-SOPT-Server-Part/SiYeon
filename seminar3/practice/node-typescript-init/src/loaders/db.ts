f6b27efe0004cfe89ce77405bd2bddaaa770a427
ksiyeon27 <siyeonk27@gmail.com>
2022-04-29 19:11:18 +0900

    after remove .git

---
+++ seminar3/practice/node-typescript-init/src/loaders/db.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import config from "../config"; 
+
+//몽고 디비에 몽구스로 연결하기! - connectDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.mongoURI); //비동기 처리
+    mongoose.set('autoCreate', true); //몽구스 옵션 변경 - 옵션은 홈페이지에서 여러개 확인하기
+    console.log("Mongoose Connected ...");
+  } catch (err: any) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
+ 
